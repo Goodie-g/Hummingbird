5f9e49895317642ace9b0d418ba97200b5c0457b
Goodie <godlucmbise@gmail.com>
2025-08-27 15:26:00 +0300

    feat: implement movie search functionality and display results in the UI

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,22 @@ fetchTrendingMovies()
     });
 
 const searchItem = document.querySelector('.js-search-item');
+const moviesSection = document.querySelector('.js-movies');
 
 document.querySelector('.js-search')
     .addEventListener('click', () => {
         searchMovie(searchItem.value)
             .then(data => {
                 console.log(data.results);
-            })
-    })
+                data.results.map(movie => {
+                    moviesSection.innerHTML += `
+                        <img src='https://image.tmdb.org/t/p/w500${movie.poster_path}' alt='${movie.title}' />
+                        <img src='https://image.tmdb.org/t/p/w500${movie.backdrop_path}' alt='${movie.title}' />    
+                        <p>${movie.title}</p>
+                        <p>${movie.release_date}</p>
+                        <p>Rating: ${movie.vote_average}</p>
+                    <
+                    `;
+                });
+            });
+    });
--- scripts/sections/movie-search.js
+++ scripts/sections/movie-search.js
@@ -1,14 +1,5 @@
 import { tmdb, endPoints } from "../axios-instance.js";
 
-/* export const searchMovie = async () => {
-    try {
-        const response = await tmdb.get(endPoints.search);
-        return response;
-    } catch (error) {
-        handleError(error)
-    }
-}; */
-
 export async function searchMovie(query) {
     try {
         const response = await tmdb.get(endPoints.search, { params: { query }, 
