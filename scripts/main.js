4781b0c3e7627e77f23e94c8c47a1a50a53e215a
Goodie <godlucmbise@gmail.com>
2025-08-29 09:04:34 +0300

    feat: add trending movies section and implement movie details page

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,26 @@
 import { fetchTrendingMovies } from "./sections/trending-movies.js";
 import { searchMovie } from "./sections/movie-search.js";
 
+const moviesSection = document.querySelector('.js-movies');
+const trendingMoviesSection = document.querySelector('.js-trending-movies');
+
 fetchTrendingMovies()
     .then(response => {
         const trendingMovies = response;
         trendingMovies.map(trendingMovie => {
-            // console.log(trendingMovie);
+            console.log(trendingMovie)
+            trendingMoviesSection.innerHTML += `
+                <img src='https://image.tmdb.org/t/p/w500${trendingMovie.poster_path}' alt='${trendingMovie.title}'/>   
+                <a href="movie.html">
+                    <p>${trendingMovie.title}</p>
+                </a> 
+                <p>${trendingMovie.release_date}</p>
+                <p>Rating: ${trendingMovie.vote_average}</p>
+            `;
         });
     });
 
 const searchItem = document.querySelector('.js-search-item');
-const moviesSection = document.querySelector('.js-movies');
 
 document.querySelector('.js-search')
     .addEventListener('click', () => {
@@ -19,12 +29,11 @@ document.querySelector('.js-search')
                 console.log(data.results);
                 data.results.map(movie => {
                     moviesSection.innerHTML += `
-                        <img src='https://image.tmdb.org/t/p/w500${movie.poster_path}' alt='${movie.title}' />
-                        <img src='https://image.tmdb.org/t/p/w500${movie.backdrop_path}' alt='${movie.title}' />    
+                        <img src='https://image.tmdb.org/t/p/w500${movie.poster_path}' alt='${movie.title}'/>   
                         <p>${movie.title}</p>
                         <p>${movie.release_date}</p>
                         <p>Rating: ${movie.vote_average}</p>
-                    <
+                    
                     `;
                 });
             });
