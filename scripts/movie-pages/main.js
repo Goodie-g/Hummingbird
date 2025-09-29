7cbdec4968504f87babd57efd97873544ede8297
Goodie <godlucmbise@gmail.com>
2025-09-29 11:45:31 +0300

    refactor: remove commented-out openMovieDetails function from main.js

--- scripts/movie-pages/main.js
+++ scripts/movie-pages/main.js
@@ -25,20 +25,6 @@ fetchUpcomingMovies()
         displayUpcomingMovies(response);
     });
 
-// export function openMovieDetails(e) {
-//     const card = e.target.closest('.js-movie-card');
-//   if (card) {
-//     const movie = {
-//       id: card.getAttribute('data-movie-id'),
-//       category: card.getAttribute('data-movie-category')
-//     };
-
-//     localStorage.setItem('selectedMovieId', movie.id);
-//     localStorage.setItem('selectedMovieCategory', movie.category);
-//     window.location.href = `movie-details-page.html`;
-//   }
-// }
-
 document.addEventListener('click', (e) => {
     openMovieDetails(e)
 });
